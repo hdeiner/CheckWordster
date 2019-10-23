9a0a892925335d7342772bf85e4b0711c1e60909
howarddeiner <howard.deiner@deinersoft.com>
2019-10-23 15:54:52 -0400

    Add login in container testing script to poll every 5 seconds until docker brings the containers to life, rather than just a nasty sleep statement tht never works well.

--- step_3_verify_in_containers.sh
+++ step_3_verify_in_containers.sh
@@ -2,19 +2,43 @@
 
 figlet -w 160 -f standard "Verify in Containers"
 
-figlet -w 160 -f small "Bring Docker Up"
-
+figlet -w 160 -f small "Starting WireMock"
 docker-compose -f docker-compose-wiremock.yml up -d
-sleep 30
 
+figlet -w 160 -f small "Wait for WireMock to start"
+
+while true ; do
+  curl -s -d "{\"numberInDigits\": \"22\"}" -H "Content-Type: application/json" -X POST localhost:9001/checkWordster > temp.txt
+  result=$(grep -c "Twenty two" temp.txt)
+  if [ $result = 1 ] ; then
+    echo "WireMock has started"
+    break
+  fi
+  sleep 5
+done
+rm temp.txt
+
+figlet -w 160 -f small "Starting CheckWordster"
 docker-compose -f docker-compose-checkwordster.yml up -d
-sleep 5
+
+figlet -w 160 -f small "Wait for CheckWordster to start"
+
+while true ; do
+  curl -s -d "{\"numberInDigits\": \"22\"}" -H "Content-Type: application/json" -X POST localhost:9002/checkWordster > temp.txt
+  result=$(grep -c "Twenty two" temp.txt)
+  if [ $result = 1 ] ; then
+    echo "CheckWordster has started"
+    break
+  fi
+  sleep 5
+done
+rm temp.txt
 
 figlet -w 160 -f small "Run Unit and Container Integration Tests"
 
 mvn test
 
-figlet -w 160 -f small "Bring Docker Down"
+figlet -w 160 -f small "Shut Down WireMock and CheckWordster"
 
 docker-compose -f docker-compose-wiremock.yml down
 docker-compose -f docker-compose-checkwordster.yml down
